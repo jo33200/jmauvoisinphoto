4f2372b00eee48ab1a3870d3d390db9eede10311
jo33200 <jo33200@gmail.com>
2025-02-07 16:53:26 +0100

    feat: add Supabase configuration and update dependencies in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nvokiklfqdtxcswazyiz.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
